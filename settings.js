40969c88cda04b392dbe1caabc154faf693049f4
udayasamantha67-source <udayasamantha67@gmail.com>
2026-02-09 11:48:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,3 +9,4 @@ SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-�
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
+  
